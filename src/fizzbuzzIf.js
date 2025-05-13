3a5421dc15b8ae855ca821d3ead1b092bdab7217
devburcu <103886137+devburcu@users.noreply.github.com>
2025-05-13 18:14:05 +0300

    FizzBuzz tamamlandı

--- src/fizzbuzzIf.js
+++ src/fizzbuzzIf.js
@@ -1,5 +1,17 @@
 function fizzbuzzIf(num) {
-    // Kodlar buraya gelecek
+    if (num % 3 === 0 && num % 5 === 0) {
+        return "FizzBuzz"
+    } else if (num % 3 === 0) {
+        return "Fizz"
+    } else if (num % 5 === 0) {
+        return "Buzz"
+    } else {
+        return num
+    }
 }
+console.log(fizzbuzzIf(30));
+console.log(fizzbuzzIf(6));
+console.log(fizzbuzzIf(10));
+console.log(fizzbuzzIf(4));
 
 module.exports = fizzbuzzIf;
--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,18 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+    switch (true){
+        case (num % 3 === 0 && num % 5 === 0):
+            return "FizzBuzz";
+        case (num % 3 === 0):
+            return "Fizz";
+        case (num % 5 === 0):
+            return "Buzz";
+        default:
+            return num
+    }
 }
+console.log(fizzbuzzSwitch(30));
+console.log(fizzbuzzSwitch(6));
+console.log(fizzbuzzSwitch(10));
+console.log(fizzbuzzSwitch(4));
 
 module.exports = fizzbuzzSwitch;
